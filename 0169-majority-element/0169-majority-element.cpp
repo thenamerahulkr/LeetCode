2361fad1124e08ef4f02874ec1bb56b7f783b486
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2023-10-29 23:17:28 +0530

    Time: 17 ms (29.32%), Space: 20 MB (18.60%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+    //         brute force method
+//         for(int i=0;i<nums.size();i++){
+//             int count=1;
+//             for(int j=i+1;j<nums.size();j++){
+//                 if(nums[i]==nums[j]){
+//                     count++;
+//                 }
+//             }
+//             if(count>nums.size()/2)
+//                 return nums[i];
+                
+//         }
+//         return -1;
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
     }
 };
