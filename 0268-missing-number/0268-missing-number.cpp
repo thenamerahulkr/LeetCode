5a4b8c641f730c7c24e99e7b52cf511dafb0f452
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2023-10-30 13:30:11 +0530

    Time: 781 ms (5.02%), Space: 18.2 MB (46.15%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+         // Outer loop that runs from 1 to N:
+        int N=nums.size();
+    for (int i = 0; i <= N; i++) {
+
+        // flag variable to check
+        //if an element exists
+        int flag = 0;
+        //Search the element using linear search:
+        for (int j = 0; j <=N - 1; j++) {
+            if (nums[j] == i) {
+
+                // i is present in the array:
+                flag = 1;
+                break;
+            }
+        }
+
+        // check if the element is missing
+        //i.e flag == 0:
+
+        if (flag == 0) return i;
+    }
+
+    // The following line will never execute.
+    // It is just to avoid warnings.
+    return -1;
+    }
+    
+};
