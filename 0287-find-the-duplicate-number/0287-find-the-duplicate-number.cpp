5e09f321c2279fdb22d3201994182df00181d09d
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2023-11-06 21:21:21 +0530

    Time: 112 ms (33.85%), Space: 61.6 MB (55.94%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+//         this is brute force solution
         int ans=0;
         sort(nums.begin(),nums.end());
         for(int i =0;i<nums.size()-1;i++){
