36f06a234086b75bf29f0333492d6a01bf5f4551
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2023-12-08 22:18:29 +0530

    Time: 97 ms (64.62%), Space: 57.6 MB (86.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -11,3 +11,14 @@ public:
         return flag;
     }
 };
+// brute force solution 
+// bool containsDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+//         for (int i = 0; i < n - 1; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[i] == nums[j])
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
