c11ae5f779d8a9385a93d5a1b18fb61ad3fc62e2
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2023-12-19 21:32:56 +0530

    Time: 4 ms (12.01%), Space: 7.3 MB (66.97%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -28,6 +28,9 @@ public:
         
         
 //         two pointer approach;
+// 1. Create two pointers slow and fast and initialize them to a head pointer.
+// 2. Move slow ptr by one step and simultaneously fast ptr by two steps until fast ptr is NULL or next of fast ptr is NULL.
+// 3.When the above condition is met, we can see that the slow ptr is pointing towards the middle of the Linked List and hence we can return the slow pointer.
         ListNode *slow=head;
         ListNode *fast=head;
         while(fast!=NULL && fast->next!=NULL){
