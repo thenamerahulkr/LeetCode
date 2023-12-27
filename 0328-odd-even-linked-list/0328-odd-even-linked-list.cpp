388ed7fce7767140ba779f66206d9153b67ad294
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2023-12-27 18:49:04 +0530

    Time: 4 ms (93.15%), Space: 10.7 MB (72.96%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -14,5 +14,27 @@ public:
         }
         odd->next=evenHead;
         return head;
+        
+        // method 2
+        // ListNode *evenHead=headhead->next;
+        // vector<int>v;
+        // while(odd!=NULL&&odd->next!=NULL){
+        //     v.push_back(odd->val);
+        //     odd=odd->next->next;
+        // }
+        // if(odd) v.push_back(odd->val);
+        // while(even!=NULL&&even->next!=NULL){
+        //     v.push_back(even->val);
+        //     even=even->next->next;
+        // }
+        // if(even) v.push_back(even->val);
+        // ListNode *temp=head;
+        // int i=0;
+        // while(i<v.size()){
+        //     temp->val=v[i];
+        //     temp=temp->next;
+        //     i++;
+        // }
+        // return head;  
     }
 };
