8c36a32717409d8d613a473e254bf66d8fd9f5b8
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2024-07-28 14:18:14 +0530

    Time: 87 ms (61.18%), Space: 38.5 MB (25.88%) - LeetHub

---
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPrime(int n)
+   {
+    if(n==1){
+            return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++)
+    {   
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+    }
+    int diagonalPrime(vector<vector<int>>& nums) {
+        int n=nums.size();
+        int mx=0;
+        for(int i =0;i<n;i++){
+            if(isPrime(nums[i][i])){
+                mx=max(mx,nums[i][i]);
+            }
+            if(isPrime(nums[i][n-i-1])){
+                mx=max(mx,nums[i][n-i-1]);
+            }
+        }
+        return mx;
+    }
+};
