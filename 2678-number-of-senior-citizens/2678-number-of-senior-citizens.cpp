5b7ef274efa70808cd035e617f6130b810411cd2
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2024-08-01 22:50:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+        for(auto:ele){
+            string result=ele.at(11)+ele.at(12)
+            int ans = int(result);
+            if(ans>60){
+                count++;
+            }
+        }
+        return count;
+    }
+};
