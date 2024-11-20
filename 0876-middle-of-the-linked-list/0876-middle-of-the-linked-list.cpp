f8cc85255f0ac90f076c97f190c53ca228c0b158
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2024-11-20 19:12:40 +0530

    Time: 0 ms (100%), Space: 9.4 MB (21.05%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,32 +11,16 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // ListNode *temp=head;
-        // int len=0;
-        // while(temp!=NULL){
-        //     temp=temp->next;
-        //     len++;
-        // }
-        // int middleIndex=len/2;
-        // int i=0;
-        // temp=head;
-        // while(i<middleIndex){
-        //     temp=temp->next;
-        //     i++;
-        // }
-        // return temp;
-        
-        
-//         two pointer approach;
-// 1. Create two pointers slow and fast and initialize them to a head pointer.
-// 2. Move slow ptr by one step and simultaneously fast ptr by two steps until fast ptr is NULL or next of fast ptr is NULL.
-// 3.When the above condition is met, we can see that the slow ptr is pointing towards the middle of the Linked List and hence we can return the slow pointer.
-        ListNode *slow=head;
-        ListNode *fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        ListNode* temp = head;
+        int length = 0;
+        while(temp!= NULL){
+            temp =  temp->next;
+            length++;
         }
-        return slow;
+        temp = head;
+        for(int i =1;i<=length/2;i++){
+                temp = temp->next;
+        }
+        return temp;
     }
 };
