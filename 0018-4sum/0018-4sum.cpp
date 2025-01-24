bbd41ede86101ba361fd9a5727f1e5e31619f2e8
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-01-24 23:01:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -12,7 +12,7 @@ public:
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
-                int p = j + 1, q = n - 1;
+                int p = j + 1, q = n - 1-1;
                 while (p < q) {
                     long long sum = (long long)nums[i] + (long long)nums[j] +
                                     (long long)nums[p] + (long long)nums[q];
