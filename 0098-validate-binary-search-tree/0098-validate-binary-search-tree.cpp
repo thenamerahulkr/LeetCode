ea55827dff38eb718b6217032f5728bc5d40db5c
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-02-25 23:33:59 +0530

    Time: 0 ms (100%), Space: 21.9 MB (47.9%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-    // void inorderTraversal(TreeNode* root, vector<int> &in) {
-    //     stack<TreeNode*> st;
-    //     TreeNode *node = root;
-    //     while(st.size()>0 or node){
-    //         if(node){
-    //             st.push(node);
-    //             node = node->left;
-    //         }
-    //         else{
-    //             TreeNode *temp = st.top();
-    //             st.pop();
-    //             in.push_back(temp->val);
-    //             node = temp->right;
-    //         }
-    //     }
-    // }
-    long long mini(TreeNode* root){
-        if(root== nullptr) return LLONG_MAX;
-        return min((long long)root->val,min(mini(root->left), mini(root->right)));
+    void inorder(TreeNode* root, TreeNode* &prev, bool &flag) {
+        if (root == NULL) return;
+        
+        inorder(root->left, prev, flag);
+        
+        if (prev != NULL) {
+            if (root->val <= prev->val) {
+                flag = false;
+                return;
+            }
+        }
+        
+        prev = root;
+        inorder(root->right, prev, flag);
     }
-    long long maxi(TreeNode* root){
-        if(root== nullptr) return LLONG_MIN;
-        return max((long long)root->val,max(maxi(root->left), maxi(root->right)));
-    }
-    bool isValidBST(TreeNode* root) {
-        // vector<int> in;
-        // inorderTraversal(root,in);
-        // for(int i =1;i<in.size(); i++){
-        //     if(in[i]<=in[i-1]) return false;
-        // }
-       if(!root) return true;
-       if (root->val > maxi(root->left) and root->val < mini(root->right)){
-         return isValidBST(root->left) and isValidBST(root->right);
-       }
-       else return false;
-      
 
+    bool isValidBST(TreeNode* root) {
+        TreeNode* prev = NULL;
+        bool flag = true;
+        inorder(root, prev, flag);
+        return flag;
     }
 };
