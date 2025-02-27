3d3664b0e320963098067ca4c128aa0b0e619af4
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-02-27 22:23:58 +0530

    Time: 0 ms (100%), Space: 15.3 MB (26.47%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
+    typedef pair<int,int> p;
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int> mp;
-        for(auto i :nums){
-            mp[i]=mp[i]+1;
+        priority_queue<p,vector<p>,greater<p>> pq;
+        // frequency array
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
         }
-        sort(nums.begin(), nums.end(), [&](int a, int b) {
-            if(mp[a] == mp[b]) return a > b; // ↓
-            else return mp[a] < mp[b]; // ↑
-        });
-        return nums;
+        for(auto x:mp){
+            int ele = x.first;
+            int freq = x.second;
+            pair<int,int> p1 ={freq,-ele};
+            pq.push(p1);
+        }
+        vector<int> ans;
+        while(pq.size()>0){
+            int times = pq.top().first;
+            int ele = -(pq.top().second);
+            pq.pop();
+            for(int i =0;i<times;i++){
+                ans.push_back(ele);
+            }
+        }
+        return ans;
     }
 };
