eacd22d087f431612984bcc64e11b9d10afc9487
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-03-22 15:20:50 +0530

    Time: 31 ms (89.97%), Space: 18.5 MB (53.96%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,50 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *tempA=headA;
-        ListNode *tempB=headB;
-        int lenA=0;
-        int lenB=0;
-        while(tempA!=NULL){
-            tempA=tempA->next;
-            lenA++;
-        }
-        while(tempB!=NULL){
-            tempB=tempB->next;
-            lenB++;
-        }
-        tempA=headA;
-        tempB=headB;
-        int diff=lenA-lenB;
-        if(lenA>lenB){
-            for(int i=1;i<=diff;i++){
-                tempA=tempA->next;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        int lenA = 0;
+        int lenB = 0;
+        while (tempA or tempB) {
+            if (tempA) {
+                tempA = tempA->next;
+                lenA++;
             }
-            while(tempA!=tempB){
-                tempA=tempA->next;
-                tempB=tempB->next;
+            if (tempB) {
+                lenB++;
+                tempB = tempB->next;
             }
-            return tempA;
         }
-//         if lenB>=lenA
-        else{
-            int diff=lenB-lenA;
-            for(int i=1;i<=diff;i++){
-                tempB=tempB->next;
+        int difference = abs(lenA - lenB);
+        tempA = headA;
+        tempB = headB;
+        if (lenA > lenB) {
+            for (int i = 0; i < difference; i++) {
+                tempA = tempA->next;
             }
-            while(tempA!=tempB){
-                tempA=tempA->next;
-                tempB=tempB->next;
+        } else {
+            for (int i = 0; i < difference; i++) {
+                tempB = tempB->next;
             }
-            return tempA;
         }
+        while (tempA != tempB) {
+            tempA = tempA->next;
+            tempB = tempB->next;
+        }
+        return tempA;
     }
 };
