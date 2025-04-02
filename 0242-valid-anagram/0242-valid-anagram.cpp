29e78822f216df6d28e16900532f7b926743e9ae
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-04-02 15:57:59 +0530

    Time: 4 ms (41.35%), Space: 9.9 MB (26.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
-        }
-        else{
-            return false;
-        }
+    if (s.length() != t.length()) return false; 
+    map<char, int> freq1, freq2;
+    for (char ch : s) freq1[ch]++; 
+    for (char ch : t) freq2[ch]++;
+    return freq1 == freq2;
     }
 };
