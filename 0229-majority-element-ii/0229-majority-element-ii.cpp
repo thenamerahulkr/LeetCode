2ddb8d057cb7f7a4433821b404e35d202554267c
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-04-28 12:54:35 +0530

    Time: 0 ms (100%), Space: 20.1 MB (9.04%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,48 +1,23 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int majority1 = 0;
-        int majority2 = 0;
-        int count1 = 0;
-        int count2 = 0;
-
-        for (const int num : nums) {
-            if (num == majority1) {
-                count1++;
-            } else if (num == majority2) {
-                count2++;
-            } else if (count1 == 0) {
-                majority1 = num;
-                count1++;
-            } else if (count2 == 0) {
-                majority2 = num;
-                count2++;
-            } else {
-                count1--;
-                count2--;
+        int n = nums.size();
+        unordered_map<int, int> freq;
+        
+        // Step 1: Count frequency of each number
+        for(int num : nums){
+            freq[num]++;
+        }
+        
+        vector<int> result;
+        
+        // Step 2: Check if frequency > n/3
+        for(auto it : freq){
+            if(it.second > n/3){
+                result.push_back(it.first);
             }
         }
-
-        count1 = 0;
-        count2 = 0;
-
-        for (const int num : nums) {
-            if (num == majority1) {
-                count1++;
-            } else if (num == majority2) {
-                count2++;
-            }
-        }
-
-        std::vector<int> res;
-
-        if (count1 > nums.size() / 3) {
-            res.push_back(majority1);
-        }
-        if (count2 > nums.size() / 3) {
-            res.push_back(majority2);
-        }
-
-        return res;        
+        
+        return result;
     }
 };
