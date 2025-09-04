adda72d6002c8d42e898e0363a149f0d031bdb5b
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-09-04 15:03:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,18 @@
 class Solution {
-private: 
-    int ways_to_climb(int n, vector<int> &dp){
-        if(n <= 2) return n;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = ways_to_climb(n - 1,dp) + ways_to_climb(n - 2,dp);  
-        return dp[n];
-    }
 public:
+    int solve(int n, int i, vector<int>& dp){
+        // abse case 
+        if(i >= n){
+            return 1;
+        }
+        if(dp[i] != -1) return dp[i];
+        int ek_wala = solve(n,i+1,dp);
+        int do_wala = solve(n, i +2, dp);
+        dp[i] = ek_wala + do_wala;
+        return d[i];
+    }
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
-        return ways_to_climb(n,dp);
+        return solve(n,1,dp);
     }
 };
