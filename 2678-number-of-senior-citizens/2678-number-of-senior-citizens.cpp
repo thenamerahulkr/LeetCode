0bc3dae262f99f99e5f892502307734ba4df0841
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-09-06 16:26:37 +0530

    Time: 0 ms (100%), Space: 17.3 MB (41.53%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int count=0;
-        for(auto:ele){
-            string result=ele.at(11)+ele.at(12)
-            int ans = int(result);
-            if(ans>60){
-                count++;
-            }
+        int n = details.size();
+        int result=0;
+        for (int i = 0; i < n; i++) {
+            string str = details[i];
+            int tens = str[11] - '0';
+int ones = str[12] - '0';
+int age = tens * 10 + ones;
+if (age > 60) result++;
         }
-        return count;
+        
+        return result;
     }
 };
