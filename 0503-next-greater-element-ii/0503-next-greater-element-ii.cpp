8df35affe28b8909bd57ca7edbcda1e393bd9800
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-07-23 15:05:26 +0530

    Time: 70 ms (7.78%), Space: 27.7 MB (99.94%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        for (int i = 0; i < n; i++) {
+           bool found = false;
+
+            // Current element ke right side mein search
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] > nums[i]) {
+                    ans[i] = nums[j];
+                    found = true;
+                    break;
+                }
+            }
+
+            // Right side mein nahi mila, tab beginning se search
+            if (!found) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] > nums[i]) {
+                        ans[i] = nums[j];
+                        found = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
