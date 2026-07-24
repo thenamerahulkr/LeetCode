6a01aede742bc5aaf0eb6ec1a443868837c73523
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-07-24 15:39:54 +0530

    Time: 115 ms (5.86%), Space: 127.7 MB (5.15%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -3,13 +3,28 @@ public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         int n = asteroids.size();
         long long m = mass;
-        sort(asteroids.begin(), asteroids.end());
-        for(int i = 0; i < n; i++){
-            if(m <= 0) return false;
-            long long asteroid = asteroids[i];
-            if(m >= asteroid){
+        // sort(asteroids.begin(), asteroids.end());
+        // for(int i = 0; i < n; i++){
+        //     if(m <= 0) return false;
+        //     long long asteroid = asteroids[i];
+        //     if(m >= asteroid){
+        //         m = m + asteroid;
+        //     }else if(m < asteroid) return false;
+        // }
+        // return true;
+
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        for (auto &asteroid : asteroids) {
+            pq.push(asteroid);
+        }
+        while (!pq.empty()) {
+            long long asteroid = pq.top();
+            pq.pop();
+
+            if (m >= asteroid) {
                 m = m + asteroid;
-            }else if(m < asteroid) return false;
+            } else if (m < asteroid)
+                return false;
         }
         return true;
     }
