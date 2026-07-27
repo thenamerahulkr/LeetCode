81b2d056c6642eae964ced72df5cab498d4b71f5
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-07-27 14:01:56 +0530

    Time: 0 ms (100%), Space: 11.2 MB (30.89%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,14 +1,37 @@
 class Solution {
+private:
+    int bs(vector<int>& nums, int x){
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        int first_idx = n;
+        while(low <= high){
+            int mid = high - low / 2;
+            if(nums[mid] >= x){
+                first_idx = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        // count of that element greater than equal to x
+        return n - first_idx;
+    }
 public:
     int specialArray(vector<int>& nums) {
+        // isko binary search se kro  ab 
+        // array sort kro pehle
         int n = nums.size();
+        sort(nums.begin(),nums.end());
         for (int i = 0; i <= n; i++) {
-            int count = 0;
-             for (int num : nums) {
-                if (num >= i) {
-                    count++;
-                }
-            }
+            // int count = 0;
+            //  for (int num : nums) {
+            //     if (num >= i) {
+            //         count++;
+            //     }
+            // }
+            int count = bs(nums,i);
             
             if (count == i) return i;
         }
