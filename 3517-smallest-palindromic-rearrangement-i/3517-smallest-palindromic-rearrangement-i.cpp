266380a5b155fb97d698fbc25e1be7dd20ce99de
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-07-28 13:37:21 +0530

    Time: 137 ms (18.24%), Space: 82 MB (7.69%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,25 +2,38 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.length();
-        // mujhe chahiye lexographically smaller
-        // store count in freq array;
-        int freq[26] = {0};
-        for(int i = 0; i < n; i++){
-            freq[s[i] - 'a']++;
-        } 
-        // freq array is ready
+        // // mujhe chahiye lexographically smaller
+        // // store count in freq array;
+        // int freq[26] = {0};
+        // for(int i = 0; i < n; i++){
+        //     freq[s[i] - 'a']++;
+        // } 
+        // // freq array is ready
+        // string left = "";
+        // string right = "";
+        // string middle = "";
+        // for(int i = 0; i < 26; i++){
+        //     left += string(freq[i] / 2, char('a' + i));
+            
+        //    if(freq[i] % 2 != 0)  middle = char('a' + i);
+            
+        // }
+        // right = left;
+        // reverse(right.begin(), right.end());
+        // return left + middle + right;
         string left = "";
         string right = "";
         string middle = "";
-        for(int i = 0; i < 26; i++){
-            left += string(freq[i] / 2, char('a' + i));
-            
-           if(freq[i] % 2 != 0)  middle = char('a' + i);
-            
+        if(n % 2 != 0) middle += s[n/2];
+        
+        for(int i = 0; i < n / 2; i++){
+            left += s[i];
         }
-        right = left;
-        reverse(right.begin(), right.end());
-        return left + middle + right;
+        sort(left.begin(), left.end());
+        for(int i = left.length() - 1; i >= 0; i--){
+            right += left[i];
+        }
 
+        return left + middle + right;
     }
 };
