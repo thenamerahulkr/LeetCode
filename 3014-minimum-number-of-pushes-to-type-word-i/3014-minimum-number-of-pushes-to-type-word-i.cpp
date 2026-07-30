74ac679fbccb32ec1a2e81ec0560fd4904e97207
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-07-30 16:08:06 +0530

    Time: 3 ms (11.73%), Space: 8.3 MB (54.67%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int min = 0;
+        int n = word.length();
+        if(n <= 8) return n;
+        else if(n <= 16 and n > 8) return 8 + (n-8) * 2;
+        else if(n > 16 and n <= 24) return 24 + (n - 16) * 3;
+        else{
+            return 48 + (n - 24) * 4;
+        }
+        return -1;
+    }
+};
