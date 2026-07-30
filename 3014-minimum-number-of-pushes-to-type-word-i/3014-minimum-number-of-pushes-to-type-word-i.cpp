c0f791ffe1cee0beb52b20b66c4c30d2f69ccd7e
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-07-30 16:17:10 +0530

    Time: 6 ms (1.39%), Space: 9 MB (16.7%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,14 +1,29 @@
-map<char, int> mp;
+
 class Solution {
 public:
     int minimumPushes(string word) {
+        // int n = word.length();
+        // if(n <= 8) return n;
+        // else if(n <= 16 and n > 8) return 8 + (n-8) * 2;
+        // else if(n > 16 and n <= 24) return 24 + (n - 16) * 3;
+        // else{
+        //     return 48 + (n - 24) * 4;
+        // }
+        // return -1;
         int n = word.length();
-        if(n <= 8) return n;
-        else if(n <= 16 and n > 8) return 8 + (n-8) * 2;
-        else if(n > 16 and n <= 24) return 24 + (n - 16) * 3;
-        else{
-            return 48 + (n - 24) * 4;
+        map<char, int> mp;
+        for(auto &ch : word){
+            mp[ch]++; 
         }
-        return -1;
+
+        // map ready hai
+        int m = mp.size();
+        int totalPushes = 0;
+        for (int i = 0; i < m; i++) {
+            int pushesPerPress = (i / 8) + 1; // 0-7: 1, 8-15: 2, 16-23: 3, 24+: 4
+            totalPushes += pushesPerPress;
+        }
+
+        return totalPushes;
     }
 };
