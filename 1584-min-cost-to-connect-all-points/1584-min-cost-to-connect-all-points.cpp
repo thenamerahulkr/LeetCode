08ccb30c111446ffd22220d61194c9724931edd0
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-03 20:05:56 +0530

    Time: 298 ms (60.87%), Space: 151.1 MB (23.32%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,26 +1,90 @@
 
+class DSU {
+private:
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    // Find ultimate parent with path compression
+    int findParent(int node) {
+        if (parent[node] == node) {
+            return node;
+        }
+
+        return parent[node] = findParent(parent[node]);
+    }
+
+    // Returns false if u and v are already connected
+    void unionByRank(int u, int v) {
+        int parentU = findParent(u);
+        int parentV = findParent(v);
+
+        if (parentU == parentV) {
+            return;
+        }
+
+        if (rank[parentU] < rank[parentV]) {
+            parent[parentU] = parentV;
+        } else if (rank[parentU] > rank[parentV]) {
+            parent[parentV] = parentU;
+        } else {
+            parent[parentV] = parentU;
+            rank[parentU]++;
+        }
+
+        return;
+    }
+};
 class Solution {
 public:
     int Prims_algo(vector<vector<pair<int, int>>>& adj) {
         int n = adj.size();
         // {weight, node}
-        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>> minHeap;
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            minHeap;
         vector<int> visited(n, 0);
         minHeap.push({0, 0});
         int mstWeight = 0;
-        while(!minHeap.empty()){
+        while (!minHeap.empty()) {
             auto [weight, node] = minHeap.top();
             minHeap.pop();
-             if (visited[node]) {
+            if (visited[node]) {
                 continue;
             }
-             visited[node] = 1;
+            visited[node] = 1;
             mstWeight += weight;
-             for (auto [neighbour, edgeWeight] : adj[node]) {
+            for (auto [neighbour, edgeWeight] : adj[node]) {
 
                 if (!visited[neighbour]) {
                     minHeap.push({edgeWeight, neighbour});
                 }
+            }
+        }
+        return mstWeight;
+    }
+    int Kruskal_algo(vector<tuple<int, int, int>>& edges, int V) {
+        int mstWeight = 0;
+        DSU dsu(V);
+        for (auto& temp : edges) {
+            int wt = get<0>(temp);
+            int u = get<1>(temp);
+            int v = get<2>(temp);
+            int pu = dsu.findParent(u);
+            int pv = dsu.findParent(v);
+            if (pu != pv) {
+                // we do union
+                dsu.unionByRank(u, v);
+                mstWeight += wt;
             }
         }
         return mstWeight;
@@ -43,6 +107,19 @@ public:
                 adj[j].push_back({i, distance});
             }
         }
-        return Prims_algo(adj);
+        vector<tuple<int, int, int>> edges;
+
+        for (int u = 0; u < n; u++) {
+            for (auto [v, wt] : adj[u]) {
+                if (u < v) {
+                    edges.push_back({wt, u, v});
+                }
+            }
+        }
+
+        sort(edges.begin(), edges.end());
+
+        // return Prims_algo(adj);
+        return Kruskal_algo(edges, n);
     }
 };
