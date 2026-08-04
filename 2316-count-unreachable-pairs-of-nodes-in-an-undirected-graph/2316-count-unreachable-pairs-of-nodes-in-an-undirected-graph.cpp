2917fdd9405349ff1ca6e1769b0df10c08322abb
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-04 19:39:27 +0530

    Time: 42 ms (77.49%), Space: 153.3 MB (65.16%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> rank;
     DSU(int n) {
         parent.resize(n);
-        rank.resize(n, 0);
+        rank.resize(n, 1);
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
