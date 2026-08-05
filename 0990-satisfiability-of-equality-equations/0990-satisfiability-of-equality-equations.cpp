a38c2b831de752f477c4c3a03dd18658bf7a3d8e
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-05 19:58:07 +0530

    Time: 0 ms (100%), Space: 15.2 MB (15.81%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,64 @@
+class DSU {
+public:
+    vector<int> parent, size;
+
+    DSU(int n) {
+        parent.resize(n + 1);
+        size.assign(n + 1, 1);
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int node) {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = find(parent[node]);
+    }
+
+    void Union(int u, int v) {
+        int pu = find(u);
+        int pv = find(v);
+
+        if (pu == pv)
+            return;
+
+        if (size[pu] < size[pv])
+            swap(pu, pv);
+
+        parent[pv] = pu;
+        size[pu] += size[pv];
+    }
+};
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        int n = equations.size();
+        DSU dsu(26);
+        for(int i = 0; i < n; i++){
+            char ch1 = equations[i][0];
+            char ch2 = equations[i][3];
+            if(equations[i][1] == '=' and equations[i][2] == '='){
+                // union
+                int u = ch1 - 'a';
+                int v = ch2 - 'a';
+                dsu.Union(u,v);
+            }
+        }
+        for(int i = 0; i < n; i++){
+           char ch1 = equations[i][0];
+            char ch2 = equations[i][3];
+            if(equations[i][1] == '!' and equations[i][2] == '='){
+                // union
+                int u = ch1 - 'a';
+                int v = ch2 - 'a';
+                int pu = dsu.find(u);
+                int pv = dsu.find(v);
+                if(pu == pv) return false;
+            }
+        }
+        return true;
+    }
+};
