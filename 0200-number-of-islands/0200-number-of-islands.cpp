6c132e235dab3d0894de9334bf30a1d91d825ee3
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-06 20:28:30 +0530

    Time: 38 ms (10.84%), Space: 23.7 MB (23.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,77 +1,81 @@
-class DSU {
-public:
-    vector<int> parent, size;
-    DSU(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-    int find(int x) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-    bool Union(int u, int v) {
-        int pu = find(u);
-        int pv = find(v);
-
-        if (pu == pv)
-            return false;
-
-        if (size[pu] < size[pv]) {
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        } else {
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-        return true;
-    }
-};
-
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
+    int dr[4] = {-1, 1, 0, 0};
+    int dc[4] = {0, 0, -1, 1};
+    void bfs(int r, int c, vector<vector<char>>& grid,
+             vector<vector<bool>>& vis) {
 
-        int m = grid.size();
-        int n = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
 
-        DSU dsu(m * n);
-        int islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1')
-                    islands++;
-            }
-        }
+        queue<pair<int, int>> q;
 
-        int dr[] = {0, 1};
-        int dc[] = {1, 0};
+        q.push({r, c});
+        vis[r][c] = true;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        while (!q.empty()) {
 
-                if (grid[i][j] == '0')
+            auto [r, c] = q.front();
+            q.pop();
+
+            for (int k = 0; k < 4; k++) {
+
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m)
                     continue;
 
-                int node = i * n + j;
+                if (!vis[nr][nc] && grid[nr][nc] == '1') {
 
-                for (int k = 0; k < 2; k++) {
+                    vis[nr][nc] = true;
+                    q.push({nr, nc});
+                }
+            }
+        }
+    }
+    void dfs(int r, int c, vector<vector<char>>& grid,
+             vector<vector<bool>>& vis) {
 
-                    int nr = i + dr[k];
-                    int nc = j + dc[k];
+        int n = grid.size();
+        int m = grid[0].size();
 
-                    if (nr >= m || nc >= n)
-                        continue;
+        vis[r][c] = true;
 
-                    if (grid[nr][nc] == '0')
-                        continue;
+        for (int k = 0; k < 4; k++) {
 
-                    int adj = nr * n + nc;
+            int nr = r + dr[k];
+            int nc = c + dc[k];
 
-                    if (dsu.Union(node, adj))
-                        islands--;
+            // Boundary check
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m)
+                continue;
+
+            // Visit only unvisited land
+            if (!vis[nr][nc] && grid[nr][nc] == '1') {
+                dfs(nr, nc, grid, vis);
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        int islands = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+
+                if (!vis[i][j] && grid[i][j] == '1') {
+
+                    islands++;
+
+                    bfs(i, j, grid, vis);
                 }
             }
         }
