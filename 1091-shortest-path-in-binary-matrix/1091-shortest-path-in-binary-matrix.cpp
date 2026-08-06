ed7b8b89fb5f374adfdf6cfc6ac8c9eb2ecc4be8
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-06 23:30:29 +0530

    Time: 10 ms (90.67%), Space: 24 MB (74.93%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
-    int dr[8] = {-1,-1,-1,0,0,1,1,1};
-    int dc[8] = {-1,0,1,-1,1,-1,0,1};
-
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-
-        int n = grid.size();
-
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
-            return -1;
-
-        queue<pair<int,int>> q;
-        vector<vector<bool>> vis(n, vector<bool>(n, false));
-
-        q.push({0,0});
+    int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+    int m_s_bfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int n) {
+        queue<pair<int, int>> q;
+        q.push({0, 0});
         vis[0][0] = true;
-
         int path = 1;
-
         while (!q.empty()) {
-
             int size = q.size();
-
             while (size--) {
-
-                auto [r,c] = q.front();
+                auto [r, c] = q.front();
                 q.pop();
-
                 // Destination reached
-                if (r == n-1 && c == n-1)
+                if (r == n - 1 && c == n - 1)
                     return path;
-
                 for (int k = 0; k < 8; k++) {
-
                     int nr = r + dr[k];
                     int nc = c + dc[k];
-
-                    if (nr < 0 || nr >= n ||
-                        nc < 0 || nc >= n)
+                    if (nr < 0 || nr >= n || nc < 0 || nc >= n)
                         continue;
-
                     if (!vis[nr][nc] && grid[nr][nc] == 0) {
-
                         vis[nr][nc] = true;
-                        q.push({nr,nc});
+                        q.push({nr, nc});
                     }
                 }
             }
-
             path++;
         }
-
         return -1;
+    }
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+            return -1;
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
+        return m_s_bfs(grid, vis, n);
     }
 };
