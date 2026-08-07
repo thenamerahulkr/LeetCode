df530baff83f6f9be582c40a426d0569b1971200
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-07 13:29:38 +0530

    Time: 265 ms (70.26%), Space: 283.6 MB (74.8%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -2,6 +2,7 @@ class DSU {
 private:
     vector<int> parent;
     vector<int> rank;
+
 public:
     DSU(int n) {
         parent.resize(n);
@@ -25,37 +26,69 @@ public:
         }
         if (rank[parentU] < rank[parentV]) {
             parent[parentU] = parentV;
-        }
-        else if (rank[parentU] > rank[parentV]) {
+        } else if (rank[parentU] > rank[parentV]) {
             parent[parentV] = parentU;
-        }
-        else {
+        } else {
             parent[parentV] = parentU;
             rank[parentU]++;
         }
     }
 
-    bool isConnected(int u, int v) {
-        return findParent(u) == findParent(v);
-    }
+    bool isConnected(int u, int v) { return findParent(u) == findParent(v); }
 };
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        DSU dsu(n);
-        int total_component = n;
-        for(auto &edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            int pu = dsu.findParent(u);
-            int pv = dsu.findParent(v);
-            if(pu != pv){
-                total_component--;
-                dsu.unionByRank(u,v);
+    void bfs(int start, vector<vector<int>>& adj, vector<bool>& visited) {
+        queue<int> q;
+        q.push(start);
+        visited[start] = true;
+        // visitt kro neighbout
+        while (!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            // visit all neighbours of current node
+            for (auto neighbour : adj[node]) {
+
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }
             }
         }
-        if(dsu.findParent(source) == dsu.findParent(destination)) return true;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source,
+                   int destination) {
+        // DSU dsu(n);
+        // int total_component = n;
+        // for(auto &edge : edges){
+        //     int u = edge[0];
+        //     int v = edge[1];
+        //     int pu = dsu.findParent(u);
+        //     int pv = dsu.findParent(v);
+        //     if(pu != pv){
+        //         total_component--;
+        //         dsu.unionByRank(u,v);
+        //     }
+        // }
+        // if(dsu.findParent(source) == dsu.findParent(destination)) return
+        // true;
 
-        return false;
-    }   
+        // return false;
+        vector<vector<int>> adj(n);
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // yhse se nowmal bfs ya dfs krna hai and number of component nikalna
+        // hai ! if 1 hai to true ni to false;
+        vector<bool> visited(n, false);
+
+        bfs(source, adj, visited);
+
+        return visited[destination];
+    }
 };
