c615d74b9f2f4501c0de61fde1b8ce44dc70faf4
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-16 17:57:11 +0530

    Time: 72 ms (90.94%), Space: 44.2 MB (83.42%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -6,29 +6,21 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>> pq;
         dist[src] = 0;
         pq.push({0, src});
-
         while (!pq.empty()) {
             auto [distance, node] = pq.top();
             pq.pop();
-
             for (auto [neighbour, weight] : adj[node]) {
-
                 if (distance + weight < dist[neighbour]) {
-
                     dist[neighbour] = distance + weight;
-
                     pq.push({dist[neighbour], neighbour});
                 }
             }
         }
-
         return dist;
     }
 
     int networkDelayTime(vector<vector<int>>& edges, int n, int k) {
-
         vector<vector<pair<int, int>>> adj(n + 1);
-
         for (auto& edge : edges) {
 
             int u = edge[0];
@@ -37,19 +29,13 @@ public:
 
             adj[u].push_back({v, wt});
         }
-
         vector<int> shortestPath = dijkstra(n, adj, k);
-
         int ans = 0;
-
         for (int node = 1; node <= n; node++) {
-
             if (shortestPath[node] == INT_MAX)
                 return -1;
-
             ans = max(ans, shortestPath[node]);
         }
-
         return ans;
     }
 };
