5674d5c591b9c028938df90544c7622025981cc4
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-19 12:43:08 +0530

    Time: 109 ms (14.53%), Space: 92.1 MB (10.47%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, std::vector<std::vector<int>>& reservedSeats) {
+        unordered_map<int, std::unordered_set<int>> reservedMap;
+
+        for (const auto& seat : reservedSeats) {
+            reservedMap[seat[0]].insert(seat[1]);
+        }
+
+        int totalGroups = (n - reservedMap.size()) * 2;
+
+        for (const auto& [row, seats] : reservedMap) {
+            bool leftBlocked = seats.count(2) || seats.count(3) || seats.count(4) || seats.count(5);
+            bool rightBlocked = seats.count(6) || seats.count(7) || seats.count(8) || seats.count(9);
+            bool middleBlocked = seats.count(4) || seats.count(5) || seats.count(6) || seats.count(7);
+
+            if (!leftBlocked && !rightBlocked) {
+                totalGroups += 2;
+            } else if (!leftBlocked || !rightBlocked || !middleBlocked) {
+                totalGroups += 1;
+            }
+        }
+
+        return totalGroups;
+    }
+};
