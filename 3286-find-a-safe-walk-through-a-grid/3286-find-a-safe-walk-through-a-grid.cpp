cb5d1e840b2aa9946730baa239027cf4f9a8c8a1
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-24 16:40:25 +0530

    Time: 14 ms (59.48%), Space: 30.4 MB (86.41%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,41 +1,67 @@
 class Solution {
 public:
-    int m, n;
-    vector<vector<int>> best;
-
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
-
-    void dfs(vector<vector<int>>& grid, int x, int y, int cost) {
-
-        // Already found a better/equal path
-        if (cost >= best[x][y])
-            return;
-
-        best[x][y] = cost;
-
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-
-            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
-                continue;
-
-            int newCost = cost + grid[nx][ny];
-
-            dfs(grid, nx, ny, newCost);
-        }
-    }
-
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
 
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        best.assign(m, vector<int>(n, INT_MAX));
+        vector<vector<int>> dist(
+            m, vector<int>(n, INT_MAX)
+        );
 
-        dfs(grid, 0, 0, grid[0][0]);
+        // {cost, {row, col}}
+        priority_queue<
+            pair<int, pair<int,int>>,
+            vector<pair<int, pair<int,int>>>,
+            greater<pair<int, pair<int,int>>>
+        > pq;
 
-        return best[m - 1][n - 1] < health;
+        dist[0][0] = grid[0][0];
+
+        pq.push({grid[0][0], {0, 0}});
+
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+
+        while (!pq.empty()) {
+
+            auto [cost, pos] = pq.top();
+            pq.pop();
+
+            int x = pos.first;
+            int y = pos.second;
+
+            // stale entry
+            if (cost > dist[x][y])
+                continue;
+
+            // reached destination
+            if (x == m - 1 && y == n - 1)
+                return cost < health;
+
+            for (int d = 0; d < 4; d++) {
+
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (nx < 0 || nx >= m ||
+                    ny < 0 || ny >= n)
+                    continue;
+
+                int newCost = cost + grid[nx][ny];
+
+                if (newCost < dist[nx][ny]) {
+
+                    dist[nx][ny] = newCost;
+
+                    pq.push({
+                        newCost,
+                        {nx, ny}
+                    });
+                }
+            }
+        }
+
+        return false;
     }
 };
