abf2139a939018840393158e96dfbed5f2b12b04
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-25 20:55:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    int recWithMemo(int n, vector<int>& dp){
+    int recWithMemo(int n, vector<int>& dp) {
         if (n == 1)
             return dp[n] = 1;
         if (n == 0)
             return dp[n] = 0;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = recWithMemo(n -1, dp) + recWithMemo(n -2, dp);
-        return dp[n];
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = recWithMemo(n - 1, dp) + recWithMemo(n - 2, dp);
+        // return dp[n];
     }
     int fib(int n) {
         vector<int> dp(31, -1);
