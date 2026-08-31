eb6df6d1dc3fc0f3ed5f825d742d8682ba19f240
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-08-31 19:00:36 +0530

    Time: 47 ms (13.58%), Space: 27.5 MB (55.83%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,49 +1,37 @@
 class Solution {
-private:
-    int solve(string& a, string& b, int i, int j, vector<vector<int>>& dp) {
-        // base case
-        if (i < 0)
-            return 0;
-        if (j < 0)
+public:
+
+    int solveWithMemo(int i, int j, string& s1, string& s2,
+                      vector<vector<int>>& dp) {
+
+        // Base case
+        if (i == s1.length() || j == s2.length())
             return 0;
 
-        // if string match
+        // Already calculated
         if (dp[i][j] != -1)
             return dp[i][j];
-        int longest_common = 0;
-        if (a[i] == b[j]) {
-            longest_common = 1 + solve(a, b, i - 1, j - 1, dp);
-        } else {
-            longest_common =
-                max(solve(a, b, i - 1, j, dp), solve(a, b, i, j - 1, dp));
+
+        // Characters match
+        if (s1[i] == s2[j]) {
+            return dp[i][j] =
+                1 + solveWithMemo(i + 1, j + 1, s1, s2, dp);
         }
-        dp[i][j] = longest_common;
-        return dp[i][j];
-    }
-    int tabular(string& a, string& b, vector<vector<int>>& dp) {
-        for (int j = 0; j <= b.size(); j++) {
-            dp[0][j] = 0;
-        }
-        for (int j = 0; j <= a.size(); j++) {
-            dp[j][0] = 0;
-        }
-        for (int i = 1; i <= a.size(); i++) {
-            for (int j = 1; j <= b.size(); j++) {
-                if (a[i-1] == b[j-1]) {
-                    dp[i][j] = 1 + dp[i - 1][j-1];
-                } else {
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return dp[a.size()][b.size()];
+
+        // Characters don't match
+        return dp[i][j] = max(
+            solveWithMemo(i + 1, j, s1, s2, dp),
+            solveWithMemo(i, j + 1, s1, s2, dp)
+        );
     }
 
-public:
-    int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.length() + 1, vector(text2.length(), -1));
-        // return solve(text1, text2, text1.length() - 1, text2.length() - 1, dp);
-        vector<vector<int>> dp1(text1.length() + 2, vector(text2.length()+2, -1));
-        return tabular(text1, text2,dp1);
+    int longestCommonSubsequence(string s1, string s2) {
+
+        int n = s1.length();
+        int m = s2.length();
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        return solveWithMemo(0, 0, s1, s2, dp);
     }
 };
