6aed6188e90727d8bb6189071e43826aa6522414
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-01 21:44:04 +0530

    Time: 66 ms (8.38%), Space: 91.8 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -7,7 +7,8 @@ public:
             return INT_MAX;
         if (k == 0)
             return 0;
-        if(dp[i][k] != -1) return dp[i][k];
+        if (dp[i][k] != -1)
+            return dp[i][k];
         int currTake = INT_MAX;
         int nextTake = INT_MAX;
         if (nums[i] <= k) {
@@ -20,10 +21,34 @@ public:
         nextTake = 0 + solve(i + 1, nums, k, dp);
         return dp[i][k] = min(currTake, nextTake);
     }
+    int solveWithTabulation(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, INT_MAX));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= k; j++) {
+                int currTake = INT_MAX;
+                int nextTake = INT_MAX;
+                if (nums[i] <= j) {
+                    int res = dp[i][j - nums[i]];
+                    if (res != INT_MAX) {
+                        currTake = 1 + res;
+                    }
+                }
+                nextTake = dp[i + 1][j];
+                dp[i][j] = min(currTake, nextTake);
+            }
+        }
+        return dp[0][k];
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-        int ans = solve(0, coins, amount, dp);
+        // int ans = solve(0, coins, amount, dp);
+        // return (ans == INT_MAX) ? -1 : ans;
+        int ans = solveWithTabulation(coins, amount);
         return (ans == INT_MAX) ? -1 : ans;
     }
 };
