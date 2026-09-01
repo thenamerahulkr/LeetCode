356677251ca6b17d682fd816823fa26a9cc3e648
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-01 15:45:04 +0530

    Time: 277 ms (6.43%), Space: 218.3 MB (5.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int solve(vector<int>& prices, int i, int k, vector<vector<int>>& dp) {
-
         if (i == prices.size() || k == 0)
             return 0;
         if (dp[i][k] != -1)
             return dp[i][k];
         int profit = INT_MIN;
-        if (k==2) {
-            int buy = -prices[i] + solve(prices, i + 1, k-1, dp);
+        if (k == 2) {
+            int buy = -prices[i] + solve(prices, i + 1, k - 1, dp);
             int skip = solve(prices, i + 1, k, dp);
             profit = max(buy, skip);
             // return dp[i][canBuy] = max(buy, skip);
-        }
-        else if(k == 1) {
+        } else if (k == 1) {
             // SELL karke transaction complete → STOP
-            int sell = prices[i] + solve(prices, i + 1, k-1, dp);
+            int sell = prices[i] + solve(prices, i + 1, k - 1, dp);
 
             // Sell nahi kiya
             int skip = solve(prices, i + 1, k, dp);
@@ -24,12 +22,33 @@ public:
         }
         return dp[i][k] = profit;
     }
+    int solveWithTabulation(vector<int>& prices, int k) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int k = 2; k >= 1; k--) {
+                if (k == 2) {
+                    int buy = -prices[i] + dp[i + 1][k - 1];
+                    int skip = dp[i + 1][k];
+                    dp[i][k] = max(buy, skip);
+                }
+                if (k == 1) {
+                    int sell = prices[i] + dp[i + 1][k - 1];
+                    int skip = dp[i + 1][k];
+                    dp[i][k] = max(sell, skip);
+                    // return dp[i][canBuy] = max(sell, skip);
+                }
+            }
+        }
+        return dp[0][2];
+    }
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         // -> k is the number of transaction
         int k = 2;
         vector<vector<int>> dp(n, vector<int>(k + 1, -1));
-        return solve(prices, 0, k, dp);
+        // return solve(prices, 0, k, dp);
+        return solveWithTabulation(prices,k);
     }
 };
