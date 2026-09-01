42f9fcd2e0d2bbbfbaa461f5dbadfc7b1feed217
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-01 15:34:42 +0530

    Time: 178 ms (6.43%), Space: 167.6 MB (5.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int min_price = INT_MAX; 
-        int max_profit = 0;     
+    int solve(vector<int>& prices, int i, int k, vector<vector<int>>& dp) {
 
-        for (int price : prices) {
-            min_price = min(min_price, price);
-            int profit = price - min_price;
-            max_profit = max(max_profit, profit);
+        if (i == prices.size() || k == 0)
+            return 0;
+        if (dp[i][k] != -1)
+            return dp[i][k];
+        int profit = INT_MIN;
+        if (k==2) {
+            int buy = -prices[i] + solve(prices, i + 1, k-1, dp);
+            int skip = solve(prices, i + 1, k, dp);
+            profit = max(buy, skip);
+            // return dp[i][canBuy] = max(buy, skip);
         }
-        return max_profit;
+        else if(k == 1) {
+            // SELL karke transaction complete → STOP
+            int sell = prices[i] + solve(prices, i + 1, k-1, dp);
+
+            // Sell nahi kiya
+            int skip = solve(prices, i + 1, k, dp);
+            profit = max(sell, skip);
+            // return dp[i][canBuy] = max(sell, skip);
+        }
+        return dp[i][k] = profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // -> k is the number of transaction
+        int k = 2;
+        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+        return solve(prices, 0, k, dp);
     }
 };
