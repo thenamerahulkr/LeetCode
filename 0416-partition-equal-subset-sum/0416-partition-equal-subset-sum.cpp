9c4b82a6d2be189a9df6333ffdd5b82b0370c225
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-01 19:21:37 +0530

    Time: 199 ms (11.55%), Space: 115 MB (13.27%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -23,22 +23,16 @@ public:
         int n = arr.size();
         vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
         dp[0][0] = true;
-        // Sum 0 can always be formed by taking nothing
         for (int i = 0; i <= n; i++) {
             dp[i][0] = true;
         }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= sum; j++) {
-                // Choice 1: Exclude the current element
                 bool notTake = dp[i - 1][j];
-
-                // Choice 2: Include the current element (if it doesn't exceed
-                // target)
                 bool take = false;
                 if (arr[i - 1] <= j) {
                     take = dp[i - 1][j - arr[i - 1]];
                 }
-
                 dp[i][j] = take || notTake;
             }
         }
