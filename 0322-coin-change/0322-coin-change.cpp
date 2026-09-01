240217131344b543e0df1207c49f1efabc0da7de
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-01 21:25:18 +0530

    Time: 55 ms (19.93%), Space: 53.9 MB (8.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int bada_number = 1e9;
-    int solve(vector<int>& coins, int amount, int index){
-        if(index >= coins.size()) return bada_number;
-        if(amount == 0) return 0;
-        if(amount < 0) return bada_number;
-        int take = 1 + solve(coins, amount - coins[index], index);
-        int not_take = solve(coins, amount, index + 1);
-        return min(take, not_take);
+    int solve(int i, vector<int>& nums, int k, vector<vector<int>>& dp) {
+        // base case
+        int n = nums.size();
+        if (i == n or k < 0)
+            return INT_MAX;
+        if (k == 0)
+            return 0;
+        if(dp[i][k] != -1) return dp[i][k];
+        int currTake = INT_MAX;
+        int nextTake = INT_MAX;
+        if (nums[i] <= k) {
+            int res = solve(i, nums, k - nums[i], dp);
+            // Prevent integer overflow if res is INT_MAX
+            if (res != INT_MAX) {
+                currTake = 1 + res;
+            }
+        }
+        nextTake = 0 + solve(i + 1, nums, k, dp);
+        return dp[i][k] = min(currTake, nextTake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        int ans = solve(coins,amount,0);
-        return ans >= bada_number ? -1 : ans;
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        int ans = solve(0, coins, amount, dp);
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
