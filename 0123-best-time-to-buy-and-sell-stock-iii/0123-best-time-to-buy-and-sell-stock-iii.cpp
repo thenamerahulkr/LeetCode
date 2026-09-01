9340b9f54328bbdfe2f65112119835013c5ca1bf
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-01 16:11:28 +0530

    Time: 236 ms (54.06%), Space: 199.6 MB (53.2%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -23,20 +23,20 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
         for (int i = n - 1; i >= 0; i--) {
-            for (int k = 2; k >= 1; k--) {
-                if (k == 2) {
+            for (int k = 4; k >= 1; k--) {
+                if (k % 2 == 0) {
                     int buy = -prices[i] + dp[i + 1][k - 1];
                     int skip = dp[i + 1][k];
                     dp[i][k] = max(buy, skip);
                 }
-                if (k == 1) {
+                if (k % 2 != 0) {
                     int sell = prices[i] + dp[i + 1][k - 1];
                     int skip = dp[i + 1][k];
                     dp[i][k] = max(sell, skip);
                 }
             }
         }
-        return dp[0][2];
+        return dp[0][4];
     }
     // TC -> O(n*k) and SC -> O(2k);
     int spaceOptimization(vector<int>& prices, int k) {
@@ -66,7 +66,8 @@ public:
         // -> k is the number of transaction
         int k = 4;
         vector<vector<int>> dp(n, vector<int>(k + 1, -1));
-        return solve(prices, 0, k, dp);
-        // return spaceOptimization(prices, k);
+        // return solve(prices, 0, k, dp);
+        return solveWithTabulation(prices, k);
+        
     }
 };
