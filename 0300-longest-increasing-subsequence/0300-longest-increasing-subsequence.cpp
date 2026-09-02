fd1c39c6c5d2e1c33fdfc8b79e7e46972d26e863
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-02 15:54:44 +0530

    Time: 898 ms (5%), Space: 405 MB (6.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int prev, vector<int>& nums, int n,
-              vector<vector<int>>& dp) {
-        // base case
+    int solveWithMemo(int i, const vector<int>& nums, int prev,
+                      vector<vector<int>>& dp) {
+        int n = nums.size();
         if (i == n)
             return 0;
-        // take
-        if (prev != -1 and dp[i][prev + 1] != -1) {
+        if (dp[i][prev + 1] != -1)
             return dp[i][prev + 1];
+        int take = 0;
+        if (prev == -1 || nums[i] > nums[prev]) {
+            take = 1 + solveWithMemo(i + 1, nums, i, dp);
         }
-        int take = INT_MIN;
-        if (prev == -1 or nums[i] > nums[prev]) {
-            take = 1 + solve(i + 1, i, nums, n, dp);
-        }
-        int skip = 0 + solve(i + 1, prev, nums, n, dp);
-        // not take
+        int skip = 0 + solveWithMemo(i + 1, nums, prev, dp);
         return dp[i][prev + 1] = max(take, skip);
-    }
-    int solveWithTabulation(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int prev = i - 1; prev >= -1; prev--) {
-                // take
-                int take = 0;
-
-                if (prev == -1 || nums[i] > nums[prev]) {
-                    take = 1 + dp[i + 1][i + 1];
-                }
-
-                // skip
-                int skip = dp[i + 1][prev + 1];
-
-                dp[i][prev + 1] = max(take, skip);
-            }
-        }
-        return dp[0][0];
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        // return solve(0, -1, nums, n, dp);
-        return solveWithTabulation(nums);
+        vector<vector<int>> dp(2501, vector<int>(2501, -1));
+        return solveWithMemo(0, nums, -1, dp);
     }
 };
