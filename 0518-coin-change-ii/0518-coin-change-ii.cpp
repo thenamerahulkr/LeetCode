38fba5c2919ec624dbb06ba6b77ba428fb6e013f
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-02 18:12:49 +0530

    Time: 43 ms (31.18%), Space: 63.2 MB (5.75%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(int i, vector<int>& nums, int k, vector<vector<int>>& dp) {
-        // base case
+        // Base Case
         int n = nums.size();
         if (i == n or k < 0)
             return 0;
@@ -19,39 +19,36 @@ public:
     }
     int solveWithTabulation(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, INT_MAX));
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
         for (int i = 0; i <= n; i++) {
-            dp[i][0] = 0;
+            dp[i][0] = 1;
         }
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j <= k; j++) {
-                int currTake = INT_MAX;
-                int nextTake = INT_MAX;
+            for (int j = 1; j <= k; j++) {
+                long long currTake = 0;
+                long long nextTake = 0;
                 if (nums[i] <= j) {
-                    int res = dp[i][j - nums[i]];
-                    if (res != INT_MAX) {
-                        currTake = 1 + res;
-                    }
+                    currTake = dp[i][j - nums[i]];
                 }
                 nextTake = dp[i + 1][j];
-                dp[i][j] = min(currTake, nextTake);
+                dp[i][j] = currTake + nextTake;
             }
         }
         return dp[0][k];
     }
     int spaceOptimization(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prev(k + 1, INT_MAX);
-        vector<int> curr(k + 1, INT_MAX);
+        vector<int> prev(k + 1, 0);
+        vector<int> curr(k + 1, 0);
         prev[0] = 0;
         curr[0] = 0;
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j <= k; j++) {
-                int currTake = INT_MAX;
-                int nextTake = INT_MAX;
+                int currTake = 0;
+                int nextTake = 0;
                 if (nums[i] <= j) {
                     int res = curr[j - nums[i]];
-                    if (res != INT_MAX) {
+                    if (res != 0) {
                         currTake = 1 + res;
                     }
                 }
@@ -65,9 +62,9 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-        int ans = solve(0, coins, amount, dp);
-        return (ans == INT_MAX) ? -1 : ans;
-        // int ans = spaceOptimization(coins, amount);
+        // int ans = solve(0, coins, amount, dp);
         // return (ans == INT_MAX) ? -1 : ans;
+        int ans = solveWithTabulation(coins, amount);
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
