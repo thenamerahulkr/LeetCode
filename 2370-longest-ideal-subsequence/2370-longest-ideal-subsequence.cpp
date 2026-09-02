aa26d4b1e0d9d81283adc9edaa22e44b55cd4e0b
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-02 23:08:47 +0530

    Time: 521 ms (10.13%), Space: 194.6 MB (24.38%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -5,17 +5,31 @@ public:
         if (i == n)
             return 0;
         int take = 0;
-        if (dp[i][prev] != -1)
-            return dp[i][prev];
-        if (prev == '\0' or abs(s[i]  - prev) <= k) {
+        if (dp[i][prev - 'a'] != -1)
+            return dp[i][prev - 'a'];
+        if (prev == '{' or abs(s[i] - prev) <= k) {
             take = 1 + solve(i + 1, s, k, s[i], dp);
         }
         int skip = 0 + solve(i + 1, s, k, prev, dp);
-        return dp[i][prev] = max(take, skip);
+        return dp[i][prev - 'a'] = max(take, skip);
     }
+    // int solveWithTabulation(string& s, int k) {
+    //     int n = s.length();
+    //     vector<vector<int>> dp(n + 1, vector<int>(123, 0));
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         for (int j = 0; j <= 123; j++) {
+    //             if (prev == '\0' or abs(s[i] - prev) <= k) {
+    //                 take = 1 + solve(i + 1, s, k, s[i], dp);
+    //             }
+    //             int skip = 0 + solve(i + 1, s, k, prev, dp);
+    //             return dp[i][prev] = max(take, skip);
+    //         }
+    //     }
+    //     return dp[0][];
+    // }
     int longestIdealString(string s, int k) {
         int n = s.length();
-        vector<vector<int>> dp(n + 1, vector<int>(123, -1));
-        return solve(0, s, k, '\0',dp);
+        vector<vector<int>> dp(n + 1, vector<int>(27, -1));
+        return solve(0, s, k, '{', dp);
     }
 };
