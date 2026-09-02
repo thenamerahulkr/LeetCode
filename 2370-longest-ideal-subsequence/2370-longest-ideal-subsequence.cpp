8db54d83fafaf45bfe8c955dcbef67dd4466278a
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-02 22:45:19 +0530

    Time: 792 ms (5.17%), Space: 458.5 MB (5.99%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -5,17 +5,17 @@ public:
         if (i == n)
             return 0;
         int take = 0;
-        if (dp[i][prev + 1] != -1)
-            return dp[i][prev + 1];
-        if (prev == -1 or abs((s[i] - 'a') - prev) <= k) {
-            take = 1 + solve(i + 1, s, k, (s[i] - 'a'), dp);
+        if (dp[i][prev] != -1)
+            return dp[i][prev];
+        if (prev == '\0' or abs(s[i]  - prev) <= k) {
+            take = 1 + solve(i + 1, s, k, s[i], dp);
         }
         int skip = 0 + solve(i + 1, s, k, prev, dp);
-        return dp[i][prev + 1] = max(take, skip);
+        return dp[i][prev] = max(take, skip);
     }
     int longestIdealString(string s, int k) {
         int n = s.length();
-        vector<vector<int>> dp(n + 1, vector<int>(27, -1));
-        return solve(0, s, k, -1,dp);
+        vector<vector<int>> dp(n + 1, vector<int>(123, -1));
+        return solve(0, s, k, '\0',dp);
     }
 };
