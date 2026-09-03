b7098deb04cc89e0d8bacbec4f07768ece6d05ce
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-03 13:33:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.28%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -5,14 +5,30 @@ public:
         return solve(n,dp);
     }
 private: 
-    int solve(int n , vector<int> &dp){
-        if(n==0) return 0;
-        if( n==1 or n==2) return 1;
-        if(dp[n] != -1) return dp[n];
-        int first_number = solve(n-1,dp);
-        int second_number = solve(n-2,dp);
-        int third_number = solve(n-3,dp);
-        dp[n] = first_number+second_number+third_number;
+    int spaceOptimization(int n){
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        
         return dp[n];
+    }
+    int solve(int n , vector<int> &dp){
+        // if(n==0) return 0;
+        // if( n==1 or n==2) return 1;
+        // if(dp[n] != -1) return dp[n];
+        // int first_number = solve(n-1,dp);
+        // int second_number = solve(n-2,dp);
+        // int third_number = solve(n-3,dp);
+        // dp[n] = first_number + second_number + third_number;
+        // return dp[n];
+        return spaceOptimization(n);
     }
 };
