b082364bfa229cf97a51395ed0ee0115af7b6f77
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-03 16:38:34 +0530

    Time: 0 ms (100%), Space: 15.9 MB (61.81%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -4,7 +4,8 @@ public:
         if (i >= arr.size()) {
             return 0;
         }
-        if(dp[i] != -1) return dp[i];
+        if (dp[i] != -1)
+            return dp[i];
         int currValue = arr[i];
         int currSum = arr[i];
         int index = i + 1;
@@ -16,12 +17,33 @@ public:
         while (index < arr.size() && arr[index] == currValue + 1) {
             index++;
         }
-        return dp[i] = max(currSum + solveWithMemo(arr, index, dp), solveWithMemo(arr, i + 1, dp));
+        return dp[i] = max(currSum + solveWithMemo(arr, index, dp),
+                           solveWithMemo(arr, i + 1, dp));
+    }
+    int solveWithTabulation(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int currValue = arr[i];
+            int currSum = arr[i];
+            int index = i + 1;
+
+            while (index < arr.size() && arr[index] == currValue) {
+                currSum += arr[i];
+                index++;
+            }
+            while (index < arr.size() && arr[index] == currValue + 1) {
+                index++;
+            }
+            dp[i] = max((currSum + dp[index]), dp[i + 1]);
+        }
+        return dp[0];
     }
     int deleteAndEarn(vector<int>& arr) {
         int n = arr.size();
         sort(arr.begin(), arr.end());
         vector<int> dp(n, -1);
-        return solveWithMemo(arr, 0, dp);
+        // return solveWithMemo(arr, 0, dp);
+        return solveWithTabulation(arr);
     }
 };
