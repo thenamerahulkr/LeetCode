5324bd7c4df634b6e45cb2774d679fd9180275a1
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-06 16:25:44 +0530

    Time: 0 ms (100%), Space: 14.6 MB (37.29%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    void dfs(int src, vector<bool>& visited, vector<vector<int>>& rooms) {
+    void bfs(int src, vector<bool>& visited, vector<vector<int>>& rooms) {
+        queue<int> q;
+        q.push(src);
         visited[src] = true;
-        for (auto& neighbour : rooms[src]) {
-            if (!visited[neighbour]) {
-                dfs(neighbour, visited, rooms);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (auto& neighbour : rooms[node]) {
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }
             }
         }
     }
+    // void dfs(int src, vector<bool>& visited, vector<vector<int>>& rooms) {
+    //     visited[src] = true;
+    //     for (auto& neighbour : rooms[src]) {
+    //         if (!visited[neighbour]) {
+    //             dfs(neighbour, visited, rooms);
+    //         }
+    //     }
+    // }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         vector<bool> visited(rooms.size(), false);
         // return all true or not
-        dfs(0, visited, rooms);
+        bfs(0, visited, rooms);
         for (bool element : visited) {
             if (element == false)
                 return false;
