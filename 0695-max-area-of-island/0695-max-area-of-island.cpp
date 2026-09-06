215bd48cec98285b7f7f0f1a02c04175a28a77c1
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-07 00:53:16 +0530

    Time: 0 ms (100%), Space: 27.4 MB (79.09%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -34,20 +34,29 @@ private:
         return area;
     }
     int dfs(int i, int j, vector<vector<int>>& grid) {
-        
         if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == 0) {
             return 0;
         }
+        int dx[4] = {-1, 1, 0, 0};
+        int dy[4] = {0, 0, -1, 1};
         int area = 1;
         grid[i][j] = 0;
-        // up
-        area += dfs(i - 1, j, grid);
-        // down
-        area += dfs(i + 1, j, grid);
-        // left
-        area += dfs(i, j - 1, grid);
-        // right
-        area += dfs(i, j + 1, grid);
+        // // up
+        // area += dfs(i - 1, j, grid);
+        // // down
+        // area += dfs(i + 1, j, grid);
+        // // left
+        // area += dfs(i, j - 1, grid);
+        // // right
+        // area += dfs(i, j + 1, grid);
+        // return area;
+        for (int d = 0; d < 4; d++) {
+            int nr = i + dx[d];
+            int nc = j + dy[d];
+            if (canWeGo(nr, nc, grid)) {
+                area += dfs(nr, nc, grid);
+            }
+        }
         return area;
     }
 
