b9bd6697fd034287a3a7df063de5f5f3ddd5554f
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-08 14:32:28 +0530

    Time: 41 ms (12.28%), Space: 48.3 MB (8%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -12,9 +12,27 @@ public:
             return memo[i][j] = solve(s, t, i - 1, j);
         }
     }
-
+    long long tabulation(string s, string t){
+        int m = s.length(), n = t.length();
+        vector<vector<unsigned long long>> dp(m + 1, vector<unsigned long long>(n + 1, 0));
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return (int)dp[m][n];
+    }
     int numDistinct(string s, string t) {
         memset(memo, -1, sizeof(memo));
-        return solve(s, t, s.length(), t.length());
+        // return solve(s, t, s.length(), t.length());
+        return tabulation(s,t);
     }
 };
