7ab0a41181a0b53d23a8ea1026b690bd1feb5f42
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-08 14:26:34 +0530

    Time: 29 ms (56.93%), Space: 13 MB (76.47%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int memo[1005][1005];
+    long long solve(string& s, string& t, int i, int j) {
+        if (j == 0) return 1;
+        if (i == 0) return 0;
+        if (memo[i][j] != -1) return memo[i][j];
+
+        if (s[i - 1] == t[j - 1]) {
+            return memo[i][j] = solve(s, t, i - 1, j - 1) + solve(s, t, i - 1, j);
+        } else {
+            return memo[i][j] = solve(s, t, i - 1, j);
+        }
+    }
+    
+    int numDistinct(string s, string t) {
+        memset(memo, -1, sizeof(memo));
+        return solve(s, t, s.length(), t.length());
+    }
+};
