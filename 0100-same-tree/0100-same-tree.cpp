9c0c9a8fc1f5d954067450ce9cac12ceb2525abb
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-12 01:45:32 +0530

    Time: 0 ms (100%), Space: 12.7 MB (77.99%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -6,14 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
- bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == 0 || q == 0) return (p == 0 && q == 0);
-        if (p->val == q->val) return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-        return false;
+    bool usingDfs(TreeNode* p, TreeNode* q) {
+        if (!p && !q)
+            return true;
+        if (!p && q)
+            return false;
+        if (p && !q)
+            return false;
+        if(p->val != q->val) return false;
+        return usingDfs(p->left, q->left) && usingDfs(p->right, q->right);
     }
+    bool isSameTree(TreeNode* p, TreeNode* q) { return usingDfs(p, q); }
 };
