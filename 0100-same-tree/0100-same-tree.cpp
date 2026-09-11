7ab78b1a1dbc9c899bbe8e58dceb2dc4c333871f
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-12 01:50:23 +0530

    Time: 0 ms (100%), Space: 12.8 MB (77.99%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -19,8 +19,33 @@ public:
             return false;
         if (p && !q)
             return false;
-        if(p->val != q->val) return false;
+        if (p->val != q->val)
+            return false;
         return usingDfs(p->left, q->left) && usingDfs(p->right, q->right);
     }
-    bool isSameTree(TreeNode* p, TreeNode* q) { return usingDfs(p, q); }
+    bool usingBfs(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> Q;
+        Q.push(p);
+        Q.push(q);
+        while (!Q.empty()) {
+            TreeNode* node1 = Q.front();
+            Q.pop();
+            TreeNode* node2 = Q.front();
+            Q.pop();
+            if (!node1 && !node2) continue;
+            if (!node1 || !node2)
+                return false;
+            if (node1->val != node2->val)
+                return false;
+            Q.push(node1->left);
+            Q.push(node2->left);
+            Q.push(node1->right);
+            Q.push(node2->right);
+        }
+        return true;
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // return usingDfs(p, q);
+        return usingBfs(p, q);
+    }
 };
