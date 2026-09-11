751fc6c5f4806a3319edd5912b87f5f644f57151
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-12 01:04:33 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.37%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    void usingStackInReverse(TreeNode* root, vector<int>& post){
+        stack<TreeNode*> st;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* current = st.top();
+            st.pop();
+            post.push_back(current->val);
+            if (current->left)
+                st.push(current->left);
+            if (current->right)
+                st.push(current->right);
+        }
+        reverse(post.begin(), post.end());
+    }
     // recursive O(n)
     void postOrder(TreeNode* root, vector<int>& post) {
         if (root == nullptr)
@@ -14,7 +28,8 @@ public:
         vector<int> post;
         if (root == nullptr)
             return post;
-        postOrder(root,post);
+        // postOrder(root,post);
+        usingStackInReverse(root, post);
         return post;
     }
 };
