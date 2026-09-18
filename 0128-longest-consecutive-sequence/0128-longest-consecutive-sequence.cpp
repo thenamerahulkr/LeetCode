beede47928a206d12481acbc478d60d926975ba9
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-18 15:50:50 +0530

    Time: 80 ms (57.34%), Space: 88.8 MB (67.54%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,49 +1,52 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        
+        unordered_set<int> st(nums.begin(), nums.end());
+        int maxLen = 0;
+        
+        for (int num : st) {
+            // Check if this number is the start of a sequence
+            if (!st.count(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+                
+                // Count consecutive numbers
+                while (st.count(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                
+                maxLen = max(maxLen, currentStreak);
+            }
+        }
+        
+        return maxLen;
+    }
+};
+
+
 // class Solution {
 // public:
 //     int longestConsecutive(vector<int>& nums) {
 //         int n = nums.size();
 //         if (n == 0)
 //             return 0;
-//         unordered_set<int> st(nums.begin(), nums.end());
+//         sort(nums.begin(), nums.end());
 //         int maxLen = 1;
-//         for (int i = 0; i < n; i++) {
-//             int number = nums[i];
-//             int currLen = 1;
-
-//             // starting point
-//             if (st.count(number + 1)) {
-//                 while (st.count(number + 1)) {
-//                     number++;
-//                     currLen++;
-//                 }
+//         int currLen = 1;
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 continue;
+//             }
+//             if (nums[i] == nums[i - 1] + 1) {
+//                 currLen++;
+//             } else {
+//                 currLen = 1;
 //             }
 //             maxLen = max(maxLen, currLen);
 //         }
 //         return maxLen;
 //     }
 // };
-
-
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0)
-            return 0;
-        sort(nums.begin(), nums.end());
-        int maxLen = 1;
-        int currLen = 1;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1]) {
-                continue;
-            }
-            if (nums[i] == nums[i - 1] + 1) {
-                currLen++;
-            } else {
-                currLen = 1;
-            }
-            maxLen = max(maxLen, currLen);
-        }
-        return maxLen;
-    }
-};
