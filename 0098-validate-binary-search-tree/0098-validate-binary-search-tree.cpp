949141c6f8d716e1800e329878327e96f8a5acef
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-22 16:44:52 +0530

    Time: 0 ms (100%), Space: 22 MB (15.9%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void inorder(TreeNode* root, TreeNode* &prev, bool &flag) {
-        if (root == NULL) return;
-        
-        inorder(root->left, prev, flag);
-        
-        if (prev != NULL) {
-            if (root->val <= prev->val) {
-                flag = false;
-                return;
-            }
-        }
-        
-        prev = root;
-        inorder(root->right, prev, flag);
+    bool validate(TreeNode* root, long long min, long long max) {
+        if (!root)
+            return true;
+        // if(!root->left && !root->right) return true;
+        if (root->val <= min || root->val >= max)
+            return false;
+        return validate(root->left, min, root->val) &&
+               validate(root->right, root->val, max);
     }
-
     bool isValidBST(TreeNode* root) {
-        TreeNode* prev = NULL;
-        bool flag = true;
-        inorder(root, prev, flag);
-        return flag;
+        // if(!root) return true;
+        // if(!root->left && !root->right) return true;
+        // int rootValue = root->val;
+
+        // if(root->left != nullptr && root->left->val >= rootValue ||
+        // root->right != nullptr &&root->right->val <= rootValue) return false;
+        // return isValidBST(root->left) && isValidBST(root->right);
+        return validate(root, LLONG_MIN, LLONG_MAX);
     }
 };
