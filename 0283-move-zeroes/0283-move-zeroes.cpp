0e59d029ff49f85a6dcd55346e19b3b503f584ce
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-24 14:45:19 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int i = 0; // Yeh track karega ki agla non-zero element kahan aana chahiye
         
-        for (int j = 0; j < nums.size(); j++) {
+        for (int j = i; j < nums.size(); j++) {
             if (nums[j] != 0) {
                 swap(nums[i], nums[j]);
                 i++;
