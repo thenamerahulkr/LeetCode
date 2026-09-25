f3ebcd4420136f8b81f90a4bb2807121fe84f5e3
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2026-09-25 12:45:21 +0530

    Time: 6 ms (21.7%), Space: 9.8 MB (25.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,6 +5,10 @@ public:
     map<char, int> freq1, freq2;
     for (char ch : s) freq1[ch]++; 
     for (char ch : t) freq2[ch]++;
+    // auto itr = freq2.begin();
+    // for(auto& it: freq1){
+    //     if(it.second != itr.second) return
+    // }
     return freq1 == freq2;
     }
 };
